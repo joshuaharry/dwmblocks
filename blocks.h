d809b1889909743986072a10e6d0dedf087d756b
Joshua Hoeflich <joshuaharry411@icloud.com>
2021-07-27 22:01:01 -0500

    Rotate blocks

--- blocks.h
+++ blocks.h
@@ -1,7 +1,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "print-volume",					    0,		     8},
 	{"", "weth",					            1800,		   0},
+	{"", "print-volume",					    0,		     8},
 	{"", "date '+%-l:%M %p'",					5,		     0},
 };
 
