e69973e6f405b61a22bc008bcb7611f68705ce6e
Joshua Hoeflich <joshuaharry411@icloud.com>
2021-07-27 22:06:20 -0500

    Clean up date command

--- blocks.h
+++ blocks.h
@@ -2,7 +2,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "weth",					            1800,		   0},
 	{"", "print-volume",					    0,		     8},
-	{"", "date '+%-l:%M %p'",					5,		     0},
+	{"", "date '+%-l:%M %p '",					5,		     0},
 };
 
 static char delim[] = " | ";
