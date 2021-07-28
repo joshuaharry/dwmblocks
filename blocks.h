d90c0a8780eae7beb515f077431d5c149f2fb213
Joshua Hoeflich <joshuaharry411@icloud.com>
2021-07-27 22:00:20 -0500

    Update bar to include weather

--- blocks.h
+++ blocks.h
@@ -1,7 +1,8 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "print-volume",					    0,		8},
-	{"", "date '+%-l:%M %p'",					5,		0},
+	{"", "print-volume",					    0,		     8},
+	{"", "weth",					            1800,		   0},
+	{"", "date '+%-l:%M %p'",					5,		     0},
 };
 
 static char delim[] = " | ";
